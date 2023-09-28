43af1f1fccf30c04c3d2d34804d5c76a647d74d7
Carmen Regina <crcarmen23@gmail.com>
2023-09-28 15:51:20 -0500

    added js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function displayTime() {
 displayTime();
 setInterval(displayTime, 1000);
 
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -17,16 +18,36 @@ setInterval(displayTime, 1000);
 //   function? How can DOM traversal be used to get the "hour-x" id of the
 //   time-block containing the button that was clicked? How might the id be
 //   useful when saving the description in local storage?
-
+$('.saveBtn').click(function(){
+  var task = $(this).siblings('.description').val()
+  var hour = $(this).parent().attr('id')
+  localStorage.setItem(hour,task)
+})
   
+
 //   TODO: Add code to apply the past, present, or future class to each time
 //   block by comparing the id to the current hour. HINTS: How can the id
 //   attribute of each time-block be used to conditionally add or remove the
 //   past, present, and future classes? How can Day.js be used to get the
 //   current hour in 24-hour time?
 
+$('.time-block').each(function (){
+var currentHour = dayjs().hour()
+var blockHour = parseInt($(this).attr('id').split('-')[1])
+if (blockHour < currentHour){
+  $(this).addClass('past')
+} else if (blockHour === currentHour){
+  $(this).addClass('present')
+} else {
+  $(this).addClass('future')
+}
+})
 
   
 //   TODO: Add code to get any user input that was saved in localStorage and set
 //   the values of the corresponding textarea elements. HINT: How can the id
 //   attribute of each time-block be used to do this?
+
+for (var i=9; i < 18; i++){
+  $('#hour-'+i+' .description').val(localStorage.getItem('hour-'+i))
+}
